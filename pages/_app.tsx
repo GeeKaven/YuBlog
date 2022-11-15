4e6d9b94a75407fe95e0a17cb0cc5665b2ff6687
GeeKaven <i@tawawa.moe>
2022-11-15 14:17:29 +0800

    feat: add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/highlighting.scss'
 import Layout from '@/components/Layout'
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar'
 import Head from 'next/head'
 import dayjs from 'dayjs'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
@@ -11,13 +12,16 @@ dayjs.extend(localizedFormat)
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme='system'>
-      <Head>
-        <meta content='width=device-width, initial-scale=1' name='viewport' />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <NextNProgress color='#6366f1' options={{ showSpinner: false }} />
+      <ThemeProvider attribute='class' defaultTheme='system'>
+        <Head>
+          <meta content='width=device-width, initial-scale=1' name='viewport' />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   )
 }
