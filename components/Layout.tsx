b66061bd72f03a036291c55e4d85f7b696b151ad
GeeKaven <i@tawawa.moe>
2022-11-16 18:20:22 +0800

    feat: header fix and blur

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import Container from './Container'
 import Footer from './Footer'
 import HeaderNav from '@/data/headerNav'
@@ -8,51 +8,29 @@ import SiteMeta from '@/data/siteMeta'
 import MobileNav from '@/components/MobileNav'
 import ThemeSwitch from '@/components/ThemeSwitch'
 import BackToTop from './BackToTop'
+import Header from './Header'
 
 type LayoutProps = {
   children?: ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [navShow, setNavShow] = useState(false)
+
+  const onToggleNav = () => setNavShow((status) => !status)
+
   return (
-    <Container>
-      <div className='flex flex-col justify-between h-screen'>
-        <header className='flex items-center justify-between py-4'>
-          <Link href='/' aria-label={SiteMeta.headerTitle}>
-            <div className='flex items-center justify-between'>
-              <div className='mr-3'>
-                <Image
-                  className='sm:inline-block mr-4 cursor-pointer dark:invert'
-                  width={48}
-                  height={48}
-                  src='/logo.svg'
-                  alt='logo'
-                />
-              </div>
-            </div>
-          </Link>
-          <div className='flex items-center text-base leading-5'>
-            <div className='hidden sm:block'>
-              {HeaderNav.map((nav) => (
-                <Link
-                  key={nav.href}
-                  href={nav.href}
-                  className='font-medium text-lg mx-1 py-2 px-4 rounded-lg leading-loose transition hover:bg-slate-200/50 dark:hover:bg-zinc-800/50'
-                >
-                  {nav.title}
-                </Link>
-              ))}
-            </div>
-            {/*Todo: MobileNav and theme switch */}
-            <ThemeSwitch />
-            <MobileNav />
-          </div>
-        </header>
-        <main className='mb-auto'>{children}</main>
-        <Footer />
-        <BackToTop />
+    <>
+      <MobileNav navShow={navShow} onToggleNav={onToggleNav} />
+      <Header onToggleNav={onToggleNav} />
+      <div className='mx-auto max-w-3xl px-4 sm:px-6 mt-[70px]'>
+        <div className='flex flex-col justify-between h-screen'>
+          <main className='mb-auto'>{children}</main>
+          <Footer />
+          <BackToTop />
+        </div>
       </div>
-    </Container>
+    </>
   )
 }
 
